3a1d7b0d9561c1c7fb7b166bd01b049f77569662
paul-11 <mrpaulyi@gmail.com>
2020-04-23 21:45:10 -0700

    Add a login credentials check without hashing feature.
    After successfully logging in, the login button will redirect you to the sign up page as a temporary end point. The end point should later be changed to the profile page's end point.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,75 @@
 import React, { Component } from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import axios from 'axios';
 
-// const Login = () => (
 export default class Login extends Component {
   constructor() {
     super();
     this.state = {
-      
+      username: '',
+      password: '',
+      isCredentialsCorrect: null
+    }
+    this.doesUsernameExist = this.doesUsernameExist.bind(this);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  doesUsernameExist() {
+    axios.get(`/search/user/${this.state.username}`)
+      .then((res) => {
+        if(res.data[0] === undefined){
+          this.setState({
+            isCredentialsCorrect: false
+          })
+        }else if (res.data[0].username === this.state.username && res.data[0].password === this.state.password) {
+          this.setState({
+            isCredentialsCorrect: true
+          })
+        } else {
+          this.setState({
+            isCredentialsCorrect: false,
+          })
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  onUsernameChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onPasswordChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  renderCredentialsError() {
+    if(this.state.isCredentialsCorrect === false){
+      return (
+        <div className="login-error">
+          Invalid username or password
+        </div>
+      )
     }
   }
 
-  componentDidMount(){
-    axios.get(`/search/user/${this.state.username}`)
-    .then((result)=>{
-      console.log(result)
-      this.setState({
-        storage: result.data
-      })
-    })
-    .catch(err=>console.log(err));
-  }
-
   render() {
+    if (this.state.isCredentialsCorrect === true) {
+      return <Redirect to='/signup' />
+      //change the '/signup' endpoint to the profile page endpoint.
+      //right now it's just redirecting you back to the sign up page if the login credentials are correct
+    }
+
     return (
       <div>
         <div className="welcome-banner-container">
           <div className="welcome-banner">
-            Welcome to App!!!!!!!!
-        </div>
+            Welcome to ThanosBook!!!!!!!!
+          </div>
         </div>
 
         <div className="form-wrapper">
@@ -36,31 +77,29 @@ export default class Login extends Component {
             <div className="input-container">
               <div className="input-heading">
                 Username
-                 </div>
+              </div>
               <div className="input-box-container">
-                <input className="input-box" />
+                <input className="input-box" name="username" onChange={this.onUsernameChange}/>
               </div>
 
               <div className="input-heading">
                 Password
-                 </div>
+              </div>
               <div className="input-box-container">
-                <input className="input-box" />
+                <input className="input-box" name="password" onChange={this.onPasswordChange}/>
               </div>
 
-              <div className="login-error">
-                Invalid username or password
-                   </div>
+              {this.renderCredentialsError()}
             </div>
 
 
             <div className="login-container">
-              <div className="login-box">
+              <div className="login-box" onClick={this.doesUsernameExist}>
                 Login
               </div>
             </div>
             <div className="create-acc-container">
-                <NavLink className="create-acc" to='/signup'>Create an account</NavLink>
+              <NavLink className="create-acc" to='/signup'>Create an account</NavLink>
             </div>
           </form>
         </div>
@@ -68,5 +107,4 @@ export default class Login extends Component {
     )
   }
 }
-// )
 
